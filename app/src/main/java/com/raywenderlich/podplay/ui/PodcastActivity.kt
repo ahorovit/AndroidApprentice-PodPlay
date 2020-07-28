23394225598ee0354497437a35889218a62c2df0
Adin Horovitz <adin@tapclicks.com>
2020-07-27 18:03:11 -0700

    Itunes Service is working with Retrofit

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -2,12 +2,24 @@ package com.raywenderlich.podplay.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+
+        val TAG = javaClass.simpleName
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        itunesRepo.searchByTerm("Android Developer") {
+            Log.i(TAG, "Results = $it")
+        }
     }
 }
