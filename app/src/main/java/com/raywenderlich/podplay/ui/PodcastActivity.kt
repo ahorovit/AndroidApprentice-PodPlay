cfde2a64388c9f911d73baf972b55bf37a15ed29
Adin Horovitz <adin@tapclicks.com>
2020-07-27 18:32:40 -0700

    Seach menu option is implemented

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.raywenderlich.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.raywenderlich.podplay.R
 import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.ItunesService
@@ -21,5 +25,17 @@ class PodcastActivity : AppCompatActivity() {
         itunesRepo.searchByTerm("Android Developer") {
             Log.i(TAG, "Results = $it")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }
